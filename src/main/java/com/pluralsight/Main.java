c7b4a49bd68733bf122f7cad75f2c2fe4f9ef295
Ahsan <sbaseer25@gmail.com>
2025-10-01 11:14:48 -0400

    added comments

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,11 +1,10 @@
 package com.pluralsight;
 
-import javax.rmi.ssl.SslRMIClientSocketFactory;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        //all variables
         String first;
         String last;
         String middle = "";
@@ -21,18 +20,22 @@ public class Main {
                 2) First Middle Last Suffix 
                 """);
         choice = scanner.nextLine().trim();
+        //Format selection
         if (choice.charAt(0) == '1' ){
             //First Way
             System.out.println("Enter your full name: ");
             fullname = scanner.nextLine().trim();
-            String[] tokens = fullname.split("\\s+");
-            if(tokens.length != 2){
+            String[] tokens = fullname.split("\\s+"); //split only on one whitespace and words not on tabs or extra whitespaces
+            if(tokens.length != 2){ //if the length of tokens is not 2 that means we do not have enough info
                 System.out.println("Invalid Input: Program will quit now...");
-                System.exit(1);
-            }
+                System.exit(1); //quit
+            }//if this does not run it means we have all the info we need
+
+            //assign
             first = tokens[0].trim();
             last = tokens[1].trim();
 
+            //print
             System.out.println("First Name: "+ first);
             System.out.println("Last Name: "+ last);
         }else{
@@ -44,11 +47,13 @@ public class Main {
                 System.out.println("Invalid Input: Program will quit now...");
                 System.exit(1);
             }
+            //assign
             first = tokens[0].trim();
             middle = tokens[1].trim();
             last = tokens[2].trim();
             suffix = tokens[3].strip();
 
+            //print
             System.out.println("First Name: "+ first);
             System.out.println("Last Name: "+ last);
             System.out.println("Middle Name: "+ middle);
